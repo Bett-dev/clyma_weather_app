821dfb5c775ea9ce7d5b8f36a51f36894b558908
Bett-dev <bettkipkiruijustus@gmail.com>
2024-11-27 21:23:05 +0300

    pull weather  data

--- lib/screens/weather.dart
+++ lib/screens/weather.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:clyma/utilities/constants.dart';
+import 'package:clyma/services/location.dart';
 
 class WeatherUi extends StatefulWidget {
   final dynamic data;
@@ -6,35 +8,102 @@ class WeatherUi extends StatefulWidget {
 
   @override
   State<WeatherUi> createState() => _WeatherUiState();
+
 }
 
 class _WeatherUiState extends State<WeatherUi> {
   late String description;
+  late double temperature;
+  late int idd;
+  late String temp;
+
+
+
   @override
   void initState() {
     super.initState();
     if (widget.data != null) {
       description = widget.data['weather'][0]['main'];
+      temperature = widget.data['main']['temp'];
+      temp = temperature.toStringAsFixed(0);
+      idd = widget.data['weather'][0]['id'];
+
+      String weatherId= Location().getWeatherIcon(idd);
+
+     
+      print('description: $description');
+      print('temperature: $temperature');
+      print('weather id: $idd');
     } else {
       description = "No Data Available";
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Center(
-          child: Text(description),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {},
+                    child: const Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temp°',
+                      style: kTempTextStyle,
+                    ),
+                    const Text(
+                      'weatherIcon',
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  'weatherMessage in cityName',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-// String description = decodedData['weather'][0]['main'];
-//       double temperature = decodedData['main']['temp'];
-//       int id = decodedData['weather'][0]['id'];
-//       print('description: $description');
-//       print('temperature: $temperature');
-//       print('weather id: $id');
